ac6c7c7bc1ef3a4f7e4513c119dca2efc9491e8c
Roman <ilovebfface@gmail.com>
2019-12-11 14:25:38 +0300

    out removed

--- out/production/CRUD_app/SQL/InsertContainerData.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO containers(
-	id, name)
-	VALUES (1, 'prototype');
-
-INSERT INTO containerdata
-SELECT 1,22,56,(SELECT CURRENT_TIME),(SELECT CURRENT_TIME)
--- out/production/CRUD_app/resources/SQL/InsertContainerData2.sql
+++
@@ -1,7 +0,0 @@
-/*
-INSERT INTO containerdata
-( containerid,temperature,humidity, internaltime, globaltime) 
-VALUES
-(1,1,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);*/
-SELECT containerid, temperature, humidity, internaltime, globaltime
-	FROM public.containerdata;
